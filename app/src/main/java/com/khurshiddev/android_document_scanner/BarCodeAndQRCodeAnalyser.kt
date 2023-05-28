ade82e63b4641bbb9a124abaf38015bcaa6865d5
khurshiddev <umaribnxattob8008@gmail.com>
2023-05-28 19:58:10 +0900

    [refactor::] BarCodeAndQRCodeAnalyser.kt

--- app/src/main/java/com/khurshiddev/android_document_scanner/BarCodeAndQRCodeAnalyser.kt
+++ app/src/main/java/com/khurshiddev/android_document_scanner/BarCodeAndQRCodeAnalyser.kt
@@ -3,9 +3,9 @@ package com.khurshiddev.android_document_scanner
 import android.annotation.SuppressLint
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
-import com.google.mlkit.vision.barcode.Barcode
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
 import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 
 class BarCodeAndQRCodeAnalyser(private val barcodeListener: BarcodeAnalyzerListener) :
